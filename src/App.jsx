b9ccde0c2793245252e0311a548cbc7da968ef8c
Hodor-Fergus <75783235+Hodor-Fergus@users.noreply.github.com>
2023-04-20 16:15:33 +0000

    adding sections

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@
 import React from "react";
 
 import About from "./Components/About";
+import Education from "./Components/Education";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
-
 import "./styles.css";
 
 /**
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "Fergus Assam",
+  title: "Data Engineer & Data Analyst",
   email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  gitHub: "Hodor-Fergus",
+  instagram: "alejandrozabadal",
+  linkedIn: "fergusassam",
+  snapchat: "lejandrozabadal",
+  twitter: "######",
+  youTube: "####",
 };
 
 const primaryColor = "#4E567E";
@@ -44,6 +44,7 @@ const App = () => {
       <Header />
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
+      <Education />
       <Portfolio />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
